4ae19432126b9dc96d91b52144e31dc71b226ef7
brianezeligg <brianezeligg@gmail.com>
2026-05-14 19:13:25 +0200

    fix supabase conflict

--- supabase.js
+++ supabase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://ihwpcecorhpjxgxwqpcv.supabase.co';
@@ -9,15 +8,3 @@ export const supabase = createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
-=======
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'https://ihwpcecorhpjxgxwqpcv.supabase.co';
-
-const SUPABASE_KEY = 'sb_publishable_8sMw2nx0rbfj1Em7YEHnzA_2tIg-7zN';
-
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
->>>>>>> 4074153965e8294811da8fb4e6a7b8f49ae45ca2
