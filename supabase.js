f53897279a6a11b59594d99516d31fca8a91b917
brianezeligg <brianezeligg@gmail.com>
2026-05-14 18:09:18 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,10 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
 const SUPABASE_URL = 'https://ihwpcecorhpjxgxwqpcv.supabase.co';
 
 const SUPABASE_KEY = 'sb_publishable_8sMw2nx0rbfj1Em7YEHnzA_2tIg-7zN';
 
-export const supabase = window.supabase.createClient(
+export const supabase = createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
